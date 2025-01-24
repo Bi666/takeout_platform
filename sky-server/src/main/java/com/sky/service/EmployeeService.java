bf8eeb82ebd5499377ad67b0fb027542844307e3
Bi666 <2488214334@qq.com>
2025-01-25 00:52:58 +0800

    feat: employee paging query

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,19 +2,28 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
     /**
-     * 员工登录
+     * Employee login
      * @param employeeLoginDTO
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
     /**
-     * 新增员工
+     * New employee
      */
     void save(EmployeeDTO employeeDTO);
+
+    /**
+     * Page query
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
